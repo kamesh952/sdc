818477de7f469da5d00262792500792249e489b1
Kamesh R <rkameshraj7@gmail.com>
2025-07-27 00:50:05 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -120,7 +120,7 @@ const Hero = () => {
                 </p>
                 {token ? (
                   <Link
-                    to="collection/:collection"
+                    to="events/create"
                     className="bg-white text-gray-950 px-8 py-3 rounded-full text-lg font-semibold shadow-md hover:shadow-lg hover:bg-gray-100 transition-all duration-300 ease-in-out transform hover:-translate-y-1"
                   >
                     Shop Now
